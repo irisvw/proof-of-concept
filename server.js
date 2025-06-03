56abf7e70993f1d95f51e87ac597a93e93ad82ce
Iris van Willigen <iris.van.willigen@gmail.com>
2025-06-03 14:09:37 +0200

    Set up server.js #2

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express';
+import { Liquid } from 'liquidjs';
+
+const app = express();
+const engine = new Liquid();
+
+app.engine('liquid', engine.express());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 8000);
+app.set('views', './views');
+
+const apiBase = "https://pokeapi.co/api/v2/";
+
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`);
+});
