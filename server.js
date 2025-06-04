bb7b6460f1fa41c4862a60c509a28d30e89d4c8e
Iris van Willigen <iris.van.willigen@gmail.com>
2025-06-04 13:02:23 +0200

    Add evolutions to evolutions tab #8

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// SETUP
 import express from 'express';
 import { Liquid } from 'liquidjs';
 
@@ -11,12 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 8000);
 app.set('views', './views');
 
-const apiBase = "https://pokeapi.co/api/v2/";
-
+// VARIABLES
+const prefix = "https://pokeapi.co/api/v2/";
 const allPokemon = await getAllPokemon();
 
+// FUNCTIONS
 async function getAllPokemon() {
-  const apiResponse = await fetch(`${apiBase}pokemon?limit=15`);
+  const apiResponse = await fetch(`${prefix}pokemon?limit=15&offset=180`);
   const apiResponseJSON = await apiResponse.json();
   let allPokemon = apiResponseJSON.results;
 
@@ -33,6 +35,18 @@ async function getPokemonDetails(pokemon) {
   pokemon.data = pokemonResponseJSON;
 }
 
+async function getEvolutionData(pokemon) {
+  let pokemonSpeciesData = await fetch(`${prefix}pokemon-species/${pokemon.name}`);
+  if (pokemonSpeciesData.status == 404) { return {} };
+
+  let pokemonSpeciesDataJSON = await pokemonSpeciesData.json();
+  let pokemonEvolutionChainURL = pokemonSpeciesDataJSON.evolution_chain.url;
+  let evolutionChainData = await fetch(pokemonEvolutionChainURL);
+
+  return await evolutionChainData.json();
+}
+
+// ROUTES
 app.get("/", async function (req, res) {
   res.render('index.liquid', {
     allPokemon: allPokemon
@@ -43,13 +57,18 @@ app.get("/:pokemon", async function (req, res) {
   let pokemon = allPokemon.find((pokemon) => pokemon.name == req.params.pokemon);
 
   if (pokemon) {
+    let evolutions = getEvolutionData(pokemon);
+
     res.render('detail.liquid', {
-    pokemon: pokemon
-  })} else {
+      pokemon: pokemon,
+      evolution: evolutions
+    });
+  } else {
     res.render('error.liquid');
   };
 });
 
+// PORT
 app.listen(app.get('port'), function () {
   console.log(`Application started on http://localhost:${app.get('port')}`);
 });
