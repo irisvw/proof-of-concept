70d8dd669c93e850043b85c2b643fc217581519b
Iris van Willigen <iris.van.willigen@gmail.com>
2025-06-09 15:06:40 +0200

    Refactor getEvolutionDetails #8

--- server.js
+++ server.js
@@ -46,43 +46,36 @@ async function getEvolutionData(pokemon) {
   return await evolutionChainData.json();
 }
 
-async function getEvolutionDetails(evolutionData) {
+function getEvolutionDetails(evolutionData) {
   let evolutionDetails = [];
   let stageTwoArray = evolutionData.chain.evolves_to;
 
-  await Promise.all(stageTwoArray.map(async (evolution) => {
-    // for each evolution, find the matching pokemon object in the allPokemon array.
-    let pokemon = await allPokemon.find((pokemon) => pokemon.name == evolution.species.name);
-    evolutionDetails.push({ name: pokemon.name, id: pokemon.data.id, type: pokemon.data.types[0].type.name, sprite: pokemon['data']['sprites']['other']['official-artwork']['front_default'], evolutions: evolution.evolves_to });
-  }));
+  stageTwoArray.forEach((evolution) => {
+    let pokemon = allPokemon.find((pokemon) => pokemon.name == evolution.species.name);
+    evolutionDetails.push({ 
+      name: pokemon.name, 
+      id: pokemon.data.id, 
+      type: pokemon.data.types[0].type.name, 
+      sprite: pokemon.data.sprites.other['official-artwork'].front_default,
+      evolutions: evolution.evolves_to 
+    });
+  });
 
-  await Promise.all(evolutionDetails.map(async (evolution) => {
-    if (evolution.evolutions.length > 0) {
-      evolution.evolutions.map(async (evo) => {
-        let pokemon = await allPokemon.find((pokemon) => pokemon.name == evo.species.name);
-        evolutionDetails.push({ name: pokemon.name, id: pokemon.data.id, type: pokemon.data.types[0].type.name, sprite: pokemon['data']['sprites']['other']['official-artwork']['front_default']});
+  evolutionDetails.forEach((pokemon) => {
+    if (pokemon.evolutions.length > 0) {
+      pokemon.evolutions.forEach((evolution) => {
+        let pokemon = allPokemon.find((pokemon) => pokemon.name == evolution.species.name);
+        evolutionDetails.push({ 
+          name: pokemon.name, 
+          id: pokemon.data.id, 
+          type: pokemon.data.types[0].type.name, 
+          sprite: pokemon.data.sprites.other['official-artwork'].front_default,
+        });
       })
     }
-  }));
+  });
 
   return evolutionDetails;
-  console.log(evolutionDetails);
-
-  // let stageThreeArray = evolutionData.chain.evolves_to;
-
-  // await Promise.all(stageTwoArray.map(async (evolution) => {
-  //   let pokemon = await allPokemon.find((pokemon) => pokemon.name == evolution.species.name);
-  //   evolutionDetails.push({ name: pokemon.name, type: pokemon.data.types[0].type.name, sprite: pokemon.data.sprites.other });
-  // }));
-
-  // get the base evolution
-  // search the original allPokemon array for that name and add the type and sprite to the object
-
-  // check if second stage
-  // loop through second stage evolutions
-  // add corresponding types and sprites
-
-  // for each 2nd stage pokemon, check if it evolves, and add to the array
 }
 
 // ROUTES
